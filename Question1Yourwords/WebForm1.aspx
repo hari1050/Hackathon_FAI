c56b73dd01bc342782391026d49a1b195ecbb1a8
Harishankar S A <103807078+hari1050@users.noreply.github.com>
2023-05-15 16:12:07 +0530

    Add files via upload

---
+++ Question1Yourwords/WebForm1.aspx
@@ -0,0 +1,63 @@
+﻿﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+
+
+        <div>
+            <h1>DICTIONARY CREATION</h1>
+            <asp:label ID="Label1" runat="server">ENTER THE KEY</asp:label> 
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+
+            <asp:label ID="Label2" runat="server">ENTER THE VALUE</asp:label> 
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+
+            <asp:Button ID="Button2" runat="server" Text="Submit" OnClick="btnSubmit_CLick" />
+        </div>
+
+        <br>
+        <hr>
+        <br>
+
+                <div>
+                    <h1>SEARCH THE DICTIONARY</h1>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="btnSearch_CLick" />
+        </div>
+    
+        <asp:label ID="lblResult" runat="server"></asp:label> 
+
+
+        <br>
+        <hr>
+        <br>
+
+        <div>
+            <asp:label ID="Label3" runat="server">DATA DISPLAY</asp:label> 
+            <asp:Button ID="Button3" runat="server" Text="Display" OnClick="btnDisplay_Click" />
+            <table id="tblDictionary" runat="server">
+                <thead>
+                    <tr>
+                       <th>My Words</th>
+                     <th>Translation</th>
+                </tr>
+                </thead>
+                <tbody>
+                   
+
+                 </tbody>
+            </table>
+            
+        </div>
+    </form>
+
+    
+</body>
+</html>
---
+++ Question1Yourwords/WebForm1.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace YourWords
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_CLick(object sender, EventArgs e)
+        {
+            string searchItem = TextBox1.Text.Trim();
+
+            Class1 myObj = new Class1();
+
+
+            string result = SearchDictionary(searchItem);
+
+            lblResult.Text = result;
+        }
+
+
+        private string SearchDictionary(string searchItem)
+        {
+            Class1 myObj = new Class1();
+            Dictionary<string, string> dictionaryData = myObj.myDict;
+
+            foreach (KeyValuePair<string, string> item in dictionaryData)
+            {
+                if (item.Key.Equals(searchItem, StringComparison.OrdinalIgnoreCase))
+                {
+                    return item.Value;
+                }
+            }
+            return "no string result found";
+        }
+
+        protected void btnSubmit_CLick(object sender, EventArgs e)
+        {
+            string key = TextBox2.Text.Trim();
+            string value = TextBox3.Text.Trim();
+
+            Class1 myObj1 = new Class1();
+            myObj1.myDict.Add(key, value);
+        }
+
+        protected void btnDisplay_Click(object sender, EventArgs e)
+        {
+            Class1 newObj = new Class1();
+            Dictionary<string, string> dictionaryData1 = newObj.myDict;
+
+            tblDictionary.Rows.Clear();
+
+            foreach (var kvp in dictionaryData1)
+            {
+                TableRow row = new TableRow();
+
+                TableCell keyCell = new TableCell();
+
+                keyCell.Text = kvp.Key;
+                row.Cells.Add(keyCell);
+
+                TableCell valueCell = new TableCell();
+                valueCell.Text = kvp.Value;
+                row.Cells.Add(valueCell);
+
+                //tblDictionary.Rows.Add(row);
+            }
+        }
+
+
+    }
+}
